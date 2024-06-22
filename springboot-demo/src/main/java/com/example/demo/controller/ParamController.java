2ac27f13e709f5ebb091b4c4bed6fd63086b0fbc
XKong-X <404910156@qq.com>
2024-06-23 00:44:17 +0800

    spring 框架注解使用与练习

--- springboot-demo/src/main/java/com/example/demo/controller/ParamController.java
+++ springboot-demo/src/main/java/com/example/demo/controller/ParamController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.Person;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -52,5 +56,78 @@ public class ParamController {
     @RequestMapping("/m7")
     public String m7(@RequestParam(required = false) List<String> listParam) {
         return "接收到的参数 listParam：" + listParam + ", 长度： " + listParam.size();
+    }
+
+    @RequestMapping("/m8")
+    public String m8(@RequestBody Person person) {
+        return "接收到的参数person：" + person.toString();
+    }
+
+    @RequestMapping("/m9/{userId}/{name}")
+    public String m9(@PathVariable Integer userId, @PathVariable("name") String username) {
+        return "userId：" + userId + ", username: " + username;
+    }
+
+    // 获取文件并保存
+    @RequestMapping("/m10")
+    public String m10(@RequestPart MultipartFile file) throws IOException {
+        System.out.println(file.getOriginalFilename());
+        file.transferTo(new File("D:\\Github\\My\\JavaEE_exercise\\springboot-demo\\temp\\"
+                + file.getOriginalFilename()));
+        return "success";
+    }
+
+    @RequestMapping("/getCookie")
+    public String getCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+//            for (Cookie cookie : cookies) {
+//                System.out.println(cookie.getName() + ":" + cookie.getValue());
+//            }
+            Arrays.stream(cookies).forEach(cookie -> {
+                System.out.println(cookie.getName() + ":" + cookie.getValue());
+            });
+        }
+        return "获取 cookie 成功";
+    }
+
+    @RequestMapping("/getCookie2")
+    public String getCookie2(@CookieValue String aaa, @CookieValue String bbb) {
+        return "cookie 中的值 aaa：" + aaa + ", bbb：" + bbb;
+    }
+
+    @RequestMapping("/setSession")
+    public String setSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.setAttribute("username", "zhangsan");
+        return "success";
+    }
+    @RequestMapping("/getSession")
+    public String getSession(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            String username = (String) session.getAttribute("username");
+            return "登录用户：" + username;
+        }
+        return "session 为空";
+    }
+    @RequestMapping("/getSession2")
+    public String getSession2(@SessionAttribute(required = false) String username) {
+        return "username: " + username;
+    }
+    @RequestMapping("/getSession3")
+    public String getSession3(HttpSession session) {
+        String username = (String) session.getAttribute("username");
+        return "登录用户：" + username;
+    }
+    @RequestMapping("/getHeader")
+    public String getHeader(HttpServletRequest request) {
+        String userAgent = request.getHeader("User-Agent");
+        return "userAgent: " + userAgent;
+    }
+
+    @RequestMapping("/getHeader2")
+    public String getHeader2(@RequestHeader("User-Agent") String userAgent) {
+        return "userAgent: " + userAgent;
     }
 }
